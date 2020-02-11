301118a2fcccac64297f9fd8800098eaa5d4815c
breuert <timo.breuer@th-koeln.de>
2020-02-11 17:27:57 +0100

    switch to old models

--- web/app/models.py
+++ web/app/models.py
@@ -44,116 +44,49 @@ def load_user(user_id):
 class Session(db.Model):
     __tablename__ = 'sessions'
     id = db.Column(db.Integer, primary_key=True)
-    start = db.Column(db.DateTime, nullable=True)
-    end = db.Column(db.DateTime, nullable=True)
-    site_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    site_user = db.Column(db.String(64), index=True)
-    system_ranking = db.Column(db.Integer, db.ForeignKey('systems.id'))
-    system_recommendation = db.Column(db.Integer, db.ForeignKey('systems.id'))
-    feedbacks = db.relationship('Feedback', backref='session', lazy='dynamic')
-
-
-class Result(db.Model):
-    __tablename__ = 'results'
-    id = db.Column(db.Integer, primary_key=True)
-    session_id = db.Column(db.Integer, db.ForeignKey('sessions.id'))
-    system_id = db.Column(db.Integer, db.ForeignKey('systems.id'))
-    feedback_id = db.Column(db.Integer, db.ForeignKey('feedbacks.id'))
-    site_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    part_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    type = db.Column(db.String(64), index=True)
-    q = db.Column(db.String(64), index=True)
-    q_date = db.Column(db.DateTime, nullable=True)
-    q_time = db.Column(db.DateTime, nullable=True)  # which datatype?
-    num_found = db.Column(db.Integer)
-    page = db.Column(db.Integer)
-    rpp = db.Column(db.Integer)
-    items = db.Column(db.JSON)
-
-    @property
-    def serialize(self):
-        return {'ranking_id': self.id,
-                'query': self.q,
-                'query_date': self.q_date,
-                'items': self.items}
-
-
-class Feedback(db.Model):
-    __tablename__ = 'feedbacks'
-    id = db.Column(db.Integer, primary_key=True)
-    start = db.Column(db.DateTime, nullable=False)
-    end = db.Column(db.DateTime, nullable=False)
-    session_id = db.Column(db.Integer, db.ForeignKey('sessions.id'))
-    site = db.Column(db.Integer, db.ForeignKey('users.id'))
-    interleave = db.Column(db.Boolean)
-    # refers to experimental and baseline ranking
-    results = db.relationship('Result', backref='feedback', lazy='dynamic')
-    # shown result list with clicks (click dates)
-    clicks = db.Column(db.JSON)
-
-
-class System(db.Model):
-    __tablename__ = 'systems'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True)
-    participant_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    type = db.Column(db.String(64), index=True)
-    results = db.relationship('Result', backref='system', lazy='dynamic')
+    start = db.Column(db.DateTime, unique=False)
+    end = db.Column(db.DateTime, unique=False)
+    rankings = db.relationship('Ranking', backref='session', lazy='dynamic')
 
     @property
     def serialize(self):
         return {'id': self.id,
-                'name': self.name,
-                'participant_id': self.participant_id,
-                'type': self.type}
+                'start': self.start,
+                'end': self.end,
+                'rankings': self.get_rankings()
+                }
+
+    def get_rankings(self):
+        return [r.id for r in self.rankings]
 
 
-# class Session(db.Model):
-#     __tablename__ = 'sessions'
-#     id = db.Column(db.Integer, primary_key=True)
-#     start = db.Column(db.DateTime, unique=False)
-#     end = db.Column(db.DateTime, unique=False)
-#     rankings = db.relationship('Ranking', backref='session', lazy='dynamic')
-#
-#     @property
-#     def serialize(self):
-#         return {'id': self.id,
-#                 'start': self.start,
-#                 'end': self.end,
-#                 'rankings': self.get_rankings()
-#                 }
-#
-#     def get_rankings(self):
-#         return [r.id for r in self.rankings]
-#
-#
-# entities = db.Table('tags',
-#     db.Column('entity_id', db.Integer, db.ForeignKey('entities.id'), primary_key=True),
-#     db.Column('ranking_id', db.Integer, db.ForeignKey('rankings.id'), primary_key=True)
-# )
-#
-#
-# class Ranking(db.Model):
-#     __tablename__ = 'rankings'
-#     id = db.Column(db.Integer, primary_key=True)
-#     textquery = db.Column(db.String(64), unique=False)
-#     session_id = db.Column(db.Integer, db.ForeignKey('sessions.id'))
-#     entities = db.relationship('Entity', secondary=entities, lazy='subquery',
-#                     backref=db.backref('rankings', lazy=True))
-#
-#     @property
-#     def serialize(self):
-#         return {'ranking_id': self.id,
-#                 'textquery': self.textquery,
-#                 'session_id': self.session_id,
-#                 'entities': self.get_entities()}
-#
-#     def get_entities(self):
-#         return [e.id for e in self.entities]
-#
-#
-# class Entity(db.Model):
-#     __tablename__ = 'entities'
-#     id = db.Column(db.String(64), primary_key=True)
-#     author = db.Column(db.String(64), unique=False)
-#     content = db.Column(db.String(64), unique=False)
+entities = db.Table('tags',
+    db.Column('entity_id', db.Integer, db.ForeignKey('entities.id'), primary_key=True),
+    db.Column('ranking_id', db.Integer, db.ForeignKey('rankings.id'), primary_key=True)
+)
+
+
+class Ranking(db.Model):
+    __tablename__ = 'rankings'
+    id = db.Column(db.Integer, primary_key=True)
+    textquery = db.Column(db.String(64), unique=False)
+    session_id = db.Column(db.Integer, db.ForeignKey('sessions.id'))
+    entities = db.relationship('Entity', secondary=entities, lazy='subquery',
+                    backref=db.backref('rankings', lazy=True))
+
+    @property
+    def serialize(self):
+        return {'ranking_id': self.id,
+                'textquery': self.textquery,
+                'session_id': self.session_id,
+                'entities': self.get_entities()}
+
+    def get_entities(self):
+        return [e.id for e in self.entities]
+
+
+class Entity(db.Model):
+    __tablename__ = 'entities'
+    id = db.Column(db.String(64), primary_key=True)
+    author = db.Column(db.String(64), unique=False)
+    content = db.Column(db.String(64), unique=False)
--- web/stella-server.py
+++ web/stella-server.py
@@ -1,13 +1,9 @@
 from flask_migrate import Migrate
 from app import create_app, db
-from app.dev import setup
 
 app = create_app('default')
 migrate = Migrate(app, db)
 
 if __name__ == '__main__':
-
-    with app.app_context():
-        setup(db)
 
     app.run(host='0.0.0.0', port=8000, debug=True)
