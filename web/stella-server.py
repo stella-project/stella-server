7eb95eacbe1e7c754b2e923cbbd71e69673e763a
breuert <timo.breuer@th-koeln.de>
2020-08-14 16:38:30 +0200

    add documentation

--- web/stella-server.py
+++ web/stella-server.py
@@ -2,8 +2,9 @@ from app import create_app, db
 from app.util import setup_db
 from flask_migrate import Migrate
 
-# app = create_app('default')
-app = create_app('postgres')
+
+# app = create_app('default')  # Use 'default' for local installation with SQLite database.
+app = create_app('postgres')  # When running in production use 'postgres'.
 migrate = Migrate(app, db)
 
 if __name__ == '__main__':
