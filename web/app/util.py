116f92227158cffb85793bf18d33df4b64b18021
jueri <jueri.keller@smail.th-koeln.de>
2020-10-07 11:58:18 +0200

    add improvements to docker-compose.yaml cration endpoint.

--- web/app/util.py
+++ web/app/util.py
@@ -1,6 +1,5 @@
 from .models import Role, System, User
-import yaml
-
+import ruamel.yaml
 def makeComposeFile():
     systems = System.query.filter_by().all()
     compose = {'version': '3',
@@ -8,7 +7,7 @@ def makeComposeFile():
             'services': {
                 'app': {
                     'build': './app', 'volumes': ['/var/run/docker.sock/:/var/run/docker.sock', './app/log:/app/log'],
-                    'ports': ['8080:8000'],
+                    'ports': "8080:8000",
                     'depends_on': [system.name for system in systems],
                     'networks': ['stella-shared']
                     }
@@ -18,8 +17,11 @@ def makeComposeFile():
         compose['services'][str(system.name)] = {
             'build' : system.url,
             'container_name' : system.name,
-            'volumes' : ['./data/:/data/']
+            'volumes' : ['./data/:/data/'],
+            'networks' : ['stella-shared']
         }
+    yaml = ruamel.yaml.YAML()
+    yaml.indent(sequence=4, offset=4)
 
     with open('docker-compose.yml', 'w') as file:
         sort_file = yaml.dump(compose, file)
@@ -60,14 +62,14 @@ def setup_db(db):
 
     db.session.commit()
 
-    ranker_a = System(status='running', name='rank_elastic', participant_id=user_part_a.id, type='RANK', submitted='DOCKER', url='https://github.com/stella-project')
-    ranker_b = System(status='submitted', name='experimental_ranker_b', participant_id=user_part_b.id, type='RANK', submitted='DOCKER', url='https://github.com/stella-project')
-    recommender_a = System(status='error', name='recom_tfidf', participant_id=user_part_a.id, type='REC', submitted='DOCKER', url='https://github.com/stella-project')
-    recommender_b = System(status='running', name='experimental_recommender_b', participant_id=user_part_b.id, type='REC', submitted='DOCKER', url='https://github.com/stella-project')
-    ranker_base_a = System(status='running', name='rank_elastic_base', participant_id=user_site_a.id, type='RANK', submitted='DOCKER', url='https://github.com/stella-project')
-    ranker_base_b = System(status='running', name='baseline_ranker_b', participant_id=user_site_b.id, type='RANK', submitted='DOCKER', url='https://github.com/stella-project')
-    recommender_base_a = System(status='running', name='recom_tfidf_base', participant_id=user_site_a.id, type='REC', submitted='DOCKER', url='https://github.com/stella-project')
-    recommender_base_b = System(status='running', name='baseline_recommender_b', participant_id=user_site_b.id, type='REC', submitted='DOCKER', url='https://github.com/stella-project')
+    ranker_a = System(status='running', name='rank_elastic', participant_id=user_part_a.id, type='RANK', submitted='DOCKER', url='https://github.com/stella-project/rank_elastic.git')
+    ranker_b = System(status='submitted', name='experimental_ranker_b', participant_id=user_part_b.id, type='RANK', submitted='DOCKER', url='https://github.com/stella-project/rank_elastic.git')
+    recommender_a = System(status='error', name='recom_tfidf', participant_id=user_part_a.id, type='REC', submitted='DOCKER', url='https://github.com/stella-project/recom_tfidf.git')
+    recommender_b = System(status='running', name='experimental_recommender_b', participant_id=user_part_b.id, type='REC', submitted='DOCKER', url='https://github.com/stella-project/recom_tfidf.git')
+    ranker_base_a = System(status='running', name='rank_elastic_base', participant_id=user_site_a.id, type='RANK', submitted='DOCKER', url='https://github.com/stella-project/rank_elastic_base.git')
+    ranker_base_b = System(status='running', name='baseline_ranker_b', participant_id=user_site_b.id, type='RANK', submitted='DOCKER', url='https://github.com/stella-project/rank_elastic_base.git')
+    recommender_base_a = System(status='running', name='recom_tfidf_base', participant_id=user_site_a.id, type='REC', submitted='DOCKER', url='https://github.com/stella-project/recom_tfidf_base.git')
+    recommender_base_b = System(status='running', name='baseline_recommender_b', participant_id=user_site_b.id, type='REC', submitted='DOCKER', url='https://github.com/stella-project/recom_tfidf_base.git')
 
     db.session.add_all([
         ranker_a,
