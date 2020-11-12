be7791e05956a2c8ec4d0f3e837095dd22371c2a
breuert <timo.breuer@th-koeln.de>
2020-11-12 20:38:07 +0100

    add selectfield for choosing site and system type

--- web/app/main/forms.py
+++ web/app/main/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, PasswordField
 from wtforms.validators import DataRequired, Length, Regexp, URL, Email, EqualTo
 from wtforms.fields.html5 import URLField
+from wtforms.fields import SelectField
 from wtforms import ValidationError
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from ..models import User
@@ -49,6 +50,7 @@ class SubmitSystem(FlaskForm):
         Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
                'Systemname must have only letters, numbers, dots or '
                'underscores')])
+    site_type = SelectField('Site & System type', choices=['GESIS (Dataset recommender)', 'LIVIVO (Document ranker)'])
     GitHubUrl = URLField('URL', validators=[DataRequired(message="Enter URL Please"),
                                             URL(message="Enter Valid URL Please.")])
 
@@ -56,13 +58,14 @@ class SubmitSystem(FlaskForm):
 
 
 class SubmitRanking(FlaskForm):
-    systemname = StringField('System Name', validators=[
+    systemname = StringField('System name', validators=[
         DataRequired(), Length(1, 64),
         Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
                'Systemname must have only letters, numbers, dots or '
                'underscores')])
-    upload = FileField('image', validators=[
-        FileRequired(),
-        FileAllowed(['out', 'txt'], 'Images only!')
-    ])
+    site_type = SelectField('Site & System type', choices=['GESIS (Dataset recommender)', 'LIVIVO (Document ranker)'])
+    upload = FileField('Run file', validators=[FileRequired(),
+                                               FileAllowed(['out', 'txt'], 'Images only!')]
+                       )
+
     submit2 = SubmitField('Submit')
