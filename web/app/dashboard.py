4664d3de17e201176ccc504c9f58c77f3e3f1d0e
Timo Breuer <47390901+breuert@users.noreply.github.com>
2021-03-10 16:22:02 +0100

    distinct between sessions and impressions

--- web/app/dashboard.py
+++ web/app/dashboard.py
@@ -38,6 +38,7 @@ class Dashboard:
         self.clicks_exp = {}
         self.feedbacks = {}
         self.impressions = {}
+        self.impressions_results = {}
         self.win = 0
         self.loss = 0
         self.tie = 0
@@ -79,6 +80,15 @@ class Dashboard:
                     self.impressions.update({date: 1})
                 else:
                     self.impressions[date] = self.impressions[date] + 1
+
+            for r in results:
+                date = r.q_date.strftime('%Y-%m-%d')
+                if self.impressions_results.get(date) is None:
+                    self.impressions_results.update({date: 1})
+                else:
+                    self.impressions_results[date] = self.impressions_results[date] + 1
+
+
             for f in self.feedbacks:
                 clicks = f.clicks
                 cnt_base = 0
@@ -123,7 +133,7 @@ class Dashboard:
 
 
             if len(self.impressions) > 0:
-                self.CTR = round(len(self.clicks_exp) / len(self.feedbacks), 4)
+                self.CTR = round(sum(self.clicks_exp.values()) / sum(self.impressions_results.values()), 4)
 
             if self.win > 0 or self.loss > 0:
                 self.outcome = "{0:.4f}".format(self.win / (self.win + self.loss))
@@ -147,11 +157,20 @@ class Dashboard:
                 dict(
                     x=list(self.impressions.keys()),
                     y=list(self.impressions.values()),
-                    type='bar'
+                    name='Sessions',
+                    type='bar',
+                    marker={'color': 'rgb(26, 118, 255)'}
+                ),
+                dict(
+                    x=list(self.impressions_results.keys()),
+                    y=list(self.impressions_results.values()),
+                    name='Impressions',
+                    type='bar',
+                    marker={'color': 'rgb(55, 83, 109)'}
                 ),
             ],
             layout=dict(
-                title='Impressions',
+                title='Sessions & Impressions',
                 margin=dict(
                     l=15,
                     r=15,
@@ -168,18 +187,20 @@ class Dashboard:
                 dict(
                     x=list(self.clicks_base.keys()),
                     y=list(self.clicks_base.values()),
-                    name='Site',
+                    name='BASE',
 
                     # type='scatter'
-                    type='bar'
+                    type='bar',
+                    marker={'color': 'rgb(26, 118, 255)'}
                 ),
                 dict(
                     x=list(self.clicks_exp.keys()),
                     y=list(self.clicks_exp.values()),
-                    name='Part',
+                    name='EXP',
 
                     # type='scatter'
-                    type='bar'
+                    type='bar',
+                    marker={'color': 'rgb(55, 83, 109)'}
                 ),
             ],
             layout=dict(
@@ -201,7 +222,8 @@ class Dashboard:
                     values=[self.win, self.loss, self.tie],
                     labels=['wins', 'loss', 'tie'],
                     hole=.3,
-                    type='pie'
+                    type='pie',
+                    marker={'colors': ['rgb(26, 118, 255)', 'rgb(55, 83, 109)', 'rgb(32, 28, 45)']}
                 ),
             ],
             layout=dict(
@@ -222,23 +244,24 @@ class Dashboard:
                 dict(type='table',
                      columnwidth=[25, 15, 100],
                      header=dict(values=['Metric', 'Value', 'Explanation'],
-
-                                 align='center', fill={"color": "#201C2D"},
+                                 align='center', fill={"color": "rgb(32, 28, 45)"},
                                  line={"width": "1", "color": 'black'},
                                  font={"family": "Arial",
                                        "size": "12",
                                        "color": "white"}),
-                     cells=dict(values=[['Win', 'Loss', 'Tie', 'Outcome', 'Impressions', 'CTR'],
-                                        [self.win, self.loss, self.tie, self.outcome, len(self.sessions), self.CTR],
+                     cells=dict(values=[['Win', 'Loss', 'Tie', 'Outcome', 'Sessions', 'Impressions', 'Clicks', 'CTR'],
+                                        [self.win, self.loss, self.tie, self.outcome, sum(self.impressions.values()), sum(self.impressions_results.values()), sum(self.clicks_exp.values()), self.CTR],
                                         ["A system 'wins' if it has more clicks on results assigned to it by the interleaving than clicks on results by the baseline system.",
                                         "Opposite of 'Win'. Number of times when the system has less clicks on results than the baseline system.",
                                         "Equal number of clicks for your system and the baseline. Only results having at least two clicks are included.",
                                         "#Wins / (#Wins + #Loss)",
                                         "Total number of sessions for which your system was used.",
+                                        "Total number of results for which your system was used.",
+                                        "Total number of clicks your system received.",
                                         "Click-through rate"]],
                                 align='center',
                                 line={"width": "1", "color": 'black'},
-                                fill={"color": "#506784"},
+                                fill={"color": "rgb(55, 83, 109)"},
                                 font={"family": "Arial",
                                       "size": "12",
                                       "color": "white"})
