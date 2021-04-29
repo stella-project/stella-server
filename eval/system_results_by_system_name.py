e4c683a13934b83ada5191d17f47ee84935a8f05
breuert <timo.breuer@th-koeln.de>
2021-04-29 14:12:10 +0200

    include imports from db

--- eval/system_results_by_system_name.py
+++ eval/system_results_by_system_name.py
@@ -1,15 +1,4 @@
-from sqlalchemy import create_engine, MetaData, Table, and_
-
-DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost:5432/postgres'
-
-engine = create_engine(DATABASE_URI, convert_unicode=True)
-metadata = MetaData(bind=engine)
-users = Table('users', metadata, autoload=True)
-systems = Table('systems', metadata, autoload=True)
-sessions = Table('sessions', metadata, autoload=True)
-feedbacks = Table('feedbacks', metadata, autoload=True)
-results = Table('results', metadata, autoload=True)
-roles = Table('roles', metadata, autoload=True)
+from db import *
 
 
 def filter_experimental_results(sys_res):
